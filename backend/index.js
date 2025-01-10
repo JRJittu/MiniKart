49daa625e214cf4fcff525e1a1ae478f05e6366f
Jitendra Rathod <122668132+JRJittu@users.noreply.github.com>
2025-01-10 21:31:57 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,3 @@
-// Packages
 import path from 'path'
 import express from 'express'
 import dotenv from 'dotenv'
@@ -12,12 +11,25 @@ import productRoutes from "./routes/productRoutes.js"
 import uploadRoutes from "./routes/uploadRoutes.js"
 import orderRoutes from "./routes/orderRoutes.js"
 
+// CORS package
+import cors from 'cors'
+
 dotenv.config()
 const port = process.env.PORT || 5000
 
 connectDB()
 
 const app = express()
+
+// CORS configuration
+const corsOptions = {
+  origin: 'https://minikart-frontend.onrender.com',  // Allow requests from your frontend domain
+  methods: 'GET, POST, PUT, DELETE',
+  credentials: true,  // If you need to handle cookies or authentication
+}
+
+// Use CORS middleware
+app.use(cors(corsOptions))
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
@@ -30,33 +42,6 @@ app.use('/api/upload', uploadRoutes)
 app.use('/api/orders', orderRoutes)
 
 const __dirname = path.resolve()
-app.use('/uploads', express.static(path.join(__dirname+"/uploads")))
+app.use('/uploads', express.static(path.join(__dirname + "/uploads")))
 
 app.listen(port, () => console.log(`Server running on the port : ${port}`))
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/* 
-app.use(express.json()): Parses incoming JSON requests and makes the payload available in req.body.
-app.use(express.urlencoded({ extended: true })): Parses incoming URL-encoded form submissions and makes the payload available in req.body.
-app.use(cookieParser()): Parses cookies from the request header and makes them available in req.cookies
-*/
