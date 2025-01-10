d14b19d6390399ef78fd0ecabaf88292fc11946c
Jitendra Rathod <122668132+JRJittu@users.noreply.github.com>
2025-01-11 00:17:01 +0530

    Update createToken.js

--- backend/utils/createToken.js
+++ backend/utils/createToken.js
@@ -7,7 +7,7 @@ const createToken = (res, userId) => {
     // Set JWT as HTTP-only cookie
     res.cookie('jwt', token, {
         httpOnly: true,
-        secure: process.env.NODE_ENV === 'development',
+        secure: true, //process.env.NODE_ENV === 'development',
         sameSite: 'strict',
         maxAge: 30 * 24 * 60 * 60 * 1000
     })
